d6be4713e0eeb7f8bb83e5f91633e4851ed0cb47
MoMoon-LKH <rlgh28@naver.com>
2022-12-07 21:23:57 +0900

    initial commit

---
+++ gmi/src/main/java/com/gameinfo/gmi/GmiApplication.java
@@ -0,0 +1,13 @@
+package com.gameinfo.gmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmiApplication.class, args);
+	}
+
+}
---
+++ gmi/src/test/java/com/gameinfo/gmi/GmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameinfo.gmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
