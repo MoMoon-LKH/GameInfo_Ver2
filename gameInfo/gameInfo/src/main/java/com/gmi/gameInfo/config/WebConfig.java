f31761140eb6c06799ce6d33a317936ea573520e
MoMoon-LKH <rlgh28@naver.com>
2023-03-05 01:28:12 +0900

    cors 에러로 인한 설정 작성

---
+++ gameInfo/gameInfo/src/main/java/com/gmi/gameInfo/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.gmi.gameInfo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://gameinfo.momoon.kro.kr"
+                );
+    }
+}
