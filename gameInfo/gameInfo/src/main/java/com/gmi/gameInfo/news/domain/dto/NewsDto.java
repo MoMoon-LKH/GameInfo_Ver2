7c729361a6ecbe75b877075aaa7898f206f68960
MoMoon-LKH <rlgh28@naver.com>
2023-05-14 01:48:44 +0900

    NewsDto CreateDate Format 처리

--- gameInfo/gameInfo/src/main/java/com/gmi/gameInfo/news/domain/dto/NewsDto.java
+++ gameInfo/gameInfo/src/main/java/com/gmi/gameInfo/news/domain/dto/NewsDto.java
@@ -1,5 +1,6 @@
 package com.gmi.gameInfo.news.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gmi.gameInfo.member.domain.dto.MemberSimpleDto;
 import com.gmi.gameInfo.platform.domain.dto.PlatformDto;
 import lombok.AllArgsConstructor;
@@ -21,6 +22,7 @@ public class NewsDto {
 
     private String content;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private Date createDate;
 
 
